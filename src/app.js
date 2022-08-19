e54b2adb8964b439be67b3248ddb28ee707f2cc4
Soim Kim <soim.kim@lge.com>
2022-08-19 09:16:37 +0900

    Change from python to node

---
+++ src/app.js
@@ -0,0 +1,5 @@
+// SPDX-FileCopyrightText: Copyright 2022 LG Electronics Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+var sample = require('./sample');
+sample.Hello();
---
+++ src/sample.js
@@ -0,0 +1,8 @@
+// SPDX-FileCopyrightText: Copyright 2022 LG Electronics Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+function printHello(){
+    console.log("Hello world");
+}
+
+module.exports.Hello = printHello;
