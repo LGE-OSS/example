1c834819f17401a5b2d681445631c2c5192aea7b
Soim <soim.kim@lge.com>
2024-06-13 10:32:15 +0900

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 // SPDX-FileCopyrightText: Copyright 2022 LG Electronics Inc.
-// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: LicenseRef-LGE-Proprietary
 
 var sample = require('./sample');
 sample.Hello();
