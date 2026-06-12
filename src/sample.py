db130759ed072ab91a48f532f15172efb1d736f4
Jkbang <81218204+bjk7119@users.noreply.github.com>
2026-06-12 15:03:04 +0900

    Delete src/sample.py

--- src/sample.py
+++
@@ -1,8 +0,0 @@
-// SPDX-FileCopyrightText: Copyright 2026 LG Electronics Inc.
-// SPDX-License-Identifier: MIT
-
-function printHello(){
-    console.log("Hello world");
-}
-
-module.exports.Hello = printHello;
