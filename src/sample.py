400844dd6ec83f501dfab138dbebc29ba76881e9
Jkbang <81218204+bjk7119@users.noreply.github.com>
2026-06-12 14:42:18 +0900

    Create sample.py

---
+++ src/sample.py
@@ -0,0 +1,8 @@
+// SPDX-FileCopyrightText: Copyright 2026 LG Electronics Inc.
+// SPDX-License-Identifier: MIT
+
+function printHello(){
+    console.log("Hello world");
+}
+
+module.exports.Hello = printHello;
