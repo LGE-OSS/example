24ab86438496a7aa19d0e2b6f8d369673e48fafd
Jiyeong Seok <50347670+dd-jy@users.noreply.github.com>
2026-09-02 11:23:17 +0900

    Update sample.js

--- src/sample.js
+++ src/sample.js
@@ -1,5 +1,5 @@
 // SPDX-FileCopyrightText: Copyright 2022 LG Electronics Inc.
-// SPDX-License-Identifier: Apache-1.1
+// SPDX-License-Identifier: Apache-2.0
 
 function printHello(){
     console.log("Hello world");
