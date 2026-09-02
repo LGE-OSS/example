89723688a75211b4fb70e7fbcf4613b1abd77a9f
Jiyeong Seok <50347670+dd-jy@users.noreply.github.com>
2026-09-02 11:11:46 +0900

    Update app.js for test

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 // SPDX-FileCopyrightText: Copyright 2022 LG Electronics Inc.
-// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: BSD-3-Clause
 
 var sample = require('./sample');
 sample.Hello();
